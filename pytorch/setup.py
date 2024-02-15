3bc3bc63c71f00abbede2d7b87f1ac7c1f96371f
sleepGuy404 <C1602842099@163.com>
2024-02-15 17:40:38 +0800

    rename

--- pytorch/setup.py
+++ pytorch/setup.py
@@ -3,17 +3,16 @@ import d2l
 
 requirements = [
     'jupyter==1.0.0',
-    'numpy==1.23.5',
-    'matplotlib==3.7.2',
-    'matplotlib-inline==0.1.6',
-    'requests==2.31.0',
-    'pandas==2.0.3'
+    'numpy==1.21.5',
+    'matplotlib==3.5.1',
+    'requests==2.25.1',
+    'pandas==1.2.4'
 ]
 
 setup(
     name='d2l',
     version=d2l.__version__,
-    python_requires='>=3.8',
+    python_requires='>=3.5',
     author='D2L Developers',
     author_email='d2l.devs@gmail.com',
     url='https://d2l.ai',
